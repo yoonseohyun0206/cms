5c48b0691486765360490599438fc4559c3ff53a
yoonseohyun0206 <ysh010206@naver.com>
2025-06-14 22:13:50 +0900

    feat: [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록 반드시 필요

--- user-api/src/test/java/com/zerobase/cms/user/service/SignupCustomerServiceTest.java
+++ user-api/src/test/java/com/zerobase/cms/user/service/SignupCustomerServiceTest.java
@@ -5,7 +5,6 @@ import com.zerobase.cms.user.domain.model.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 
